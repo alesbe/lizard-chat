9cda76bd17bab12d9753e90311b2bfd543b81702
alesbe <aesparzabellver@gmail.com>
2022-05-16 21:29:16 +0200

    Setup basic socket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
+import React, { useEffect } from 'react';
 import './App.css';
+const io = require("socket.io-client");
 
 function App() {
+  useEffect(() => {
+    const socket = io(`http://localhost:8000`);
+
+    socket.on('connect', () => {
+      console.log("Connecting to server...");
+
+      socket.emit("enterChat", { name: "test" });
+    })
+  }, []);
+
   return (
     <div className="App">
       <h1>Lizard chat</h1>
--- server/server.js
+++ server/server.js
@@ -1,8 +1,30 @@
-const express = require('express')
-const app = express()
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+const io = require('socket.io')(server, { cors: { origin: "*" } });
 
+// Express config
+app.use(cors());
 app.get('/', function (req, res) {
-  res.send('Lizard Chat')
+  res.send('Lizard Chat').status(200);
 })
 
-app.listen(8000)
+// Socket.IO config
+io.on('connection', (socket) => {
+    console.log("New client connected!");
+
+    socket.on('enterChat', (req) => {
+        console.log(req.name);
+    })
+    socket.on('dissconnect', () => {
+        console.log("Client disconnected");
+    })
+});
+
+// Server initialization
+const port = process.env.PORT || 8000;
+server.listen(port, (err) => {
+    if (err) throw err;
+
+    console.log(`Server running at port ${port}`)
+});
